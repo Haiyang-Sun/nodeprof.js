5854b6209f259674653e958506f954d5e38689d5
Alexander Jordan <alexander.jordan@oracle.com>
2020-03-02 16:50:59 +1000

    Adapt to upstream JSConfig change

--- src/ch.usi.inf.nodeprof/src/ch/usi/inf/nodeprof/utils/RawEventsTracingSupport.java
+++ src/ch.usi.inf.nodeprof/src/ch/usi/inf/nodeprof/utils/RawEventsTracingSupport.java
@@ -40,7 +40,7 @@ import com.oracle.truffle.js.nodes.instrumentation.JSTags.ReadPropertyTag;
 import com.oracle.truffle.js.nodes.instrumentation.JSTags.ReadVariableTag;
 import com.oracle.truffle.js.nodes.instrumentation.JSTags.WritePropertyTag;
 import com.oracle.truffle.js.nodes.instrumentation.JSTags.WriteVariableTag;
-import com.oracle.truffle.js.runtime.JSTruffleOptions;
+import com.oracle.truffle.js.runtime.JSConfig;
 import com.oracle.truffle.js.runtime.builtins.JSFunction;
 import com.oracle.truffle.js.runtime.objects.JSObject;
 
@@ -62,7 +62,7 @@ public class RawEventsTracingSupport {
                             StandardTags.ExpressionTag.class,
                             JSTags.InputNodeTag.class).build();
             instrumenter.attachExecutionEventFactory(sourceSectionFilter, inputGeneratingObjects, getFactory());
-            Logger.info("Low-level event tracing enabled [SVM: " + JSTruffleOptions.SubstrateVM + "]");
+            Logger.info("Low-level event tracing enabled [SVM: " + JSConfig.SubstrateVM + "]");
             enabled = true;
         }
     }
