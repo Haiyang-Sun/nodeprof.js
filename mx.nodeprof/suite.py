b5521efdb1a384012c68e728cdc553d23dc86609
Alexander Jordan <alexander.jordan@oracle.com>
2021-11-08 15:17:33 +0100

    Update `javaCompliance` in suite

--- mx.nodeprof/suite.py
+++ mx.nodeprof/suite.py
@@ -40,7 +40,7 @@ suite = {
         "graal-js:GRAALJS"
       ],
       "annotationProcessors" : ["truffle:TRUFFLE_DSL_PROCESSOR"],
-      "javaCompliance" : "1.8",
+      "javaCompliance" : "8+",
       "workingSets" : "Truffle,JavaScript",
       "checkstyle" : "ch.usi.inf.nodeprof",
     },
@@ -52,7 +52,7 @@ suite = {
         "mx:JUNIT"
       ],
       "annotationProcessors" : ["truffle:TRUFFLE_DSL_PROCESSOR"],
-      "javaCompliance" : "1.8",
+      "javaCompliance" : "8+",
       "workingSets" : "Truffle,JavaScript,Test",
       "testProject": True,
       "checkstyle" : "ch.usi.inf.nodeprof",
